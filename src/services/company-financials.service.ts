b586197dc6b726a8cf0456ae4307aec8be7ee30f
Ndumiso <accounts@tybo.co.za>
2025-08-21 16:20:32 +0200

    feat: Enhance CompanyFinancialsService with detailed ICompanyFinancials interface and update method signatures for improved type safety

--- src/services/company-financials.service.ts
+++ src/services/company-financials.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { INode } from '../models/schema';
+// Company Financials interface
+export interface ICompanyFinancials {
+  id: number;
+  company_id: number;
+  period_date: string;
+  year: number;
+  month: number;
+  quarter: number;
+  turnover: string | null;
+  cost_of_sales: string | null;
+  business_expenses: string | null;
+  gross_profit: string | null;
+  net_profit: string | null;
+  cash_in_hand: string | null;
+  debtors_outstanding: string | null;
+  creditors_outstanding: string | null;
+  inventory_value: string | null;
+  working_capital_ratio: string | null;
+  net_assets: string | null;
+  created_at: string;
+  updated_at: string;
+}
 import { Constants } from './service';
 
 @Injectable({ providedIn: 'root' })
@@ -10,36 +31,36 @@ export class CompanyFinancialsService {
 
   constructor(private http: HttpClient) {}
 
-  addCompanyFinancials(data: any): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/add-company-financials.php`, data);
+  addCompanyFinancials(data: Partial<ICompanyFinancials>): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/add-company-financials.php`, data);
   }
 
-  upsertCompanyFinancials(data: any): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/upsert-company-financials.php`, data);
+  upsertCompanyFinancials(data: Partial<ICompanyFinancials>): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/upsert-company-financials.php`, data);
   }
 
-  updateCompanyFinancials(id: number, data: any): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/update-company-financials.php`, { id, ...data });
+  updateCompanyFinancials(id: number, data: Partial<ICompanyFinancials>): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/update-company-financials.php`, { id, ...data });
   }
 
-  getCompanyFinancialsById(id: number): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/get-company-financials.php`, { id });
+  getCompanyFinancialsById(id: number): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/get-company-financials.php`, { id });
   }
 
-  getCompanyFinancialsByPeriod(company_id: number, period_date: string): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/get-company-financials-by-period.php`, { company_id, period_date });
+  getCompanyFinancialsByPeriod(company_id: number, period_date: string): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/get-company-financials-by-period.php`, { company_id, period_date });
   }
 
-  listCompanyFinancials(filters: any = {}): Observable<INode<any>[]> {
-    return this.http.post<INode<any>[]>(`${this.apiUrl}/list-company-financials.php`, filters);
+  listCompanyFinancials(filters: any = {}): Observable<ICompanyFinancials[]> {
+    return this.http.post<ICompanyFinancials[]>(`${this.apiUrl}/list-company-financials.php`, filters);
   }
 
   countCompanyFinancials(filters: any = {}): Observable<{ count: number }> {
     return this.http.post<{ count: number }>(`${this.apiUrl}/count-company-financials.php`, filters);
   }
 
-  latestCompanyFinancials(company_id: number): Observable<INode<any>> {
-    return this.http.post<INode<any>>(`${this.apiUrl}/latest-company-financials.php`, { company_id });
+  latestCompanyFinancials(company_id: number): Observable<ICompanyFinancials> {
+    return this.http.post<ICompanyFinancials>(`${this.apiUrl}/latest-company-financials.php`, { company_id });
   }
 
   deleteCompanyFinancials(id: number): Observable<{ success: boolean }> {
